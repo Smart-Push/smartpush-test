a5897e8516ccbbc9e71ba629a7fc3eb27f011a3d
Lukasz SWIDER <lukasz.swider.p@gmail.com>
2024-03-29 11:21:39 +0100

    feat(front): init mui

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,16 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider } from "@emotion/react";
+import {
+  createTheme,
+  CssBaseline,
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+} from "@mui/material";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            SmartPush Test
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Box>
+    </Container>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
