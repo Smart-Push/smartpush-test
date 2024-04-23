bfe717c6a2cff561d6e87daedff3b8b460d09cf1
Simon <simon@cheztoi.eu>
2024-04-23 17:27:53 +0200

    api(entities): Add transaction and type payment

---
+++ api/src/Entity/Transaction.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\TransactionRepository;
+
+#[ORM\Entity(repositoryClass: TransactionRepository::class)]
+class Transaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\Column]
+    private ?float $amount = null;
+
+    #[ORM\ManyToOne]
+    private ?TypePayment $typePayment = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $categoryId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getTypePayment(): ?TypePayment
+    {
+        return $this->typePayment;
+    }
+
+    public function setTypePayment(?TypePayment $typePayment): static
+    {
+        $this->typePayment = $typePayment;
+
+        return $this;
+    }
+
+    public function getCategoryId(): ?int
+    {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId(?int $categoryId): static
+    {
+        $this->categoryId = $categoryId;
+
+        return $this;
+    }
+}
